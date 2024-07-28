194aa6a11d9b6f3fda6acc5323ed45bc6297afde
Jisoo Shin <110219986+kelly6226@users.noreply.github.com>
2024-07-28 19:34:02 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,19 @@
+num1 = int(input())
+num2 = input()
+
+### num2의 백의 자리
+firstNum = int(num2[0])
+secondNum = int(num2[1])
+thirdNum = int(num2[2])
+
+### (3)번 출력
+print(num1 * thirdNum)
+
+### (4)번 출력
+print(num1 * secondNum)
+
+### (5) 출력
+print(num1 * firstNum)
+
+### (6) 출력
+print(num1 * int(num2))
